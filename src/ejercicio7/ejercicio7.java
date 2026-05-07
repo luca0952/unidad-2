7f40962a6258a642c12c8fb1c3b5209762a5fe1c
Your Name <you@example.com>
2026-05-07 10:01:39 -0300

    actualizacion

--- src/ejercicio7/ejercicio7.java
+++ src/ejercicio7/ejercicio7.java
@@ -6,36 +6,31 @@ public class ejercicio7 {
 
 	public static void main(String[] args) {
 		Scanner entrada = new Scanner(System.in);
-		
-         int cantProducto = entrada.nextInt(); 
-         
-      String producto = entrada.next();
-      
+		String producto;
+		int cantProducto;
 		 do {
 		System.out.println("Ingrese un producto: ");
-		
+		producto = entrada.nextLine();
+		 while (producto.isBlank()) {
+	            System.err.println("Error: El producto no puede quedar vacío. Inténtelo de nuevo:");
+	            producto = entrada.nextLine();
+	        }
 		System.out.println("Ingrese la cantidad: ");
-		
+		cantProducto = entrada.nextInt(); 
 			
 		System.out.println("El ultimo producto ingresado fue: "+ producto +" y su cantidad era "+ cantProducto);
-			
-		
-		if (cantProducto < 5) {
-			
-			System.err.println("Alerta ⚠️⚠⚠️ : Este producto  se encuentra en stock bajo⚠️⚠️⚠️: ");
-			
-			
-		}	
-		else if (cantProducto == 5) {
-			
-			System.out.println("este producto se encuentra en un stock normal: ");
-			
-			}
+						
+		if(cantProducto < 5 ) {	System.err.println("Alerta ⚠️⚠⚠️ : Este producto  se encuentra en stock bajo⚠️⚠️⚠️: ");
+		} else {System.out.println("Este producto tiene stock");
+		}
 		
 		}while(cantProducto < 0 || cantProducto > 130);
-		System.err.println("🚫 Error: la cantidad de "+ producto +"no es valida. Ingrese un valor entre 0 y 130. ");
-		  
-		  System.out.println("Ingrese el nombre del producto: ");
-	        String nombre = entrada.nextLine();
+		 
+		 if (cantProducto < 0 || cantProducto > 130) {
+			 System.err.println("🚫 Error: la cantidad de "+ producto +"no es valida. Ingrese un valor entre 0 y 130. ");
+		 }else {	  
+		  System.out.println("Nombre de el Producto: " + producto );
+		  System.out.println("La cantidad es:" + cantProducto);
+		 }
 	}
 		}
